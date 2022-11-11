a1375918db2431bd93532f78cfc0e5d8be9d5891
joaofranciscopm <78879881+joaofranciscopm@users.noreply.github.com>
2022-11-11 15:48:12 -0300

    adaptando login
    login e senha validados com sucesso

--- gestao_fenox/lib/homepage.dart
+++ gestao_fenox/lib/homepage.dart
@@ -4,7 +4,7 @@ import 'package:validatorless/validatorless.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -24,80 +24,82 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 300,
-                  height: 250,
-                  child: Image.asset("assets/FenoxTec2.png"),
+  Widget build(BuildContext context) => Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                width: 350,
+                height: 300,
+                child: Image.asset("assets/FenoxTec2.png"),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Usuário',
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  ),
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                TextFormField(
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.left,
+                validator: Validatorless.multiple([
+                  Validatorless.required(
+                    'Preencha seu usuário',
+                  ),
+                  Validatorless.min(6, 'Usuário incorreto ou senha incorreta'),
+                ]),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  obscureText: true,
                   style: TextStyle(
                     fontWeight: FontWeight.normal,
                     color: Color.fromARGB(255, 0, 0, 0),
                     fontSize: 20,
                   ),
                   textAlign: TextAlign.left,
-                  decoration: InputDecoration(labelText: 'Usuário'),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(labelText: 'Senha'),
                   validator: Validatorless.multiple([
-                    Validatorless.required(
-                      'Preencha seu usuário',
-                    ),
+                    Validatorless.required('Senha obrigatória'),
                     Validatorless.min(
                         6, 'Usuário incorreto ou senha incorreta'),
-                  ]),
+                  ])),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 40,
+                width: 100,
+                child: ElevatedButton(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Entrar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ]),
+                  onPressed: () {
+                    var formValid = _formKey.currentState?.validate() ?? false;
+                    if (formValid) {
+                      Navigator.pushNamed(context, '/backoffice');
+                    }
+                  },
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                    obscureText: true,
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 20,
-                    ),
-                    textAlign: TextAlign.left,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(labelText: 'Senha'),
-                    validator: Validatorless.multiple([
-                      Validatorless.required('Senha obrigatória'),
-                      Validatorless.min(
-                          6, 'Usuário incorreto ou senha incorreta'),
-                    ])),
-                SizedBox(
-                  height: 80,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      var formValid =
-                          _formKey.currentState?.validate() ?? false;
-                      if (formValid) {
-                        Navigator.pushNamed(context, '/backoffice');
-                      }
-                    },
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Entrar",
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                            textAlign: TextAlign.center,
-                          )
-                        ]))
-              ],
-            )));
-  }
+              )
+            ],
+          )));
 }
